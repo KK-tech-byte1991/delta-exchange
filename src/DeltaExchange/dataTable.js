2a0ed67ff688b36f03b6bb9502d6a8220af4412b
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2022-07-16 12:54:26 +0530

    websocket implemented with symbols array

--- src/DeltaExchange/dataTable.js
+++ src/DeltaExchange/dataTable.js
@@ -1,64 +1,69 @@
-import React, { useState, useEffect } from 'react';
-
-import { AgGridReact } from 'ag-grid-react';
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
 import axios from "axios";
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-// const socket = io('wss://production-esocket.delta.exchange')
-const ws = new WebSocket('wss://production-esocket.delta.exchange')
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+const ws = new WebSocket("wss://production-esocket.delta.exchange");
 
 const Datatable = () => {
-    useEffect(() => {
-        axios({
-            // Endpoint to send files
-            url: "https://api.delta.exchange/v2/products",
-            method: "GET",
+  const [rowData, setRowData] = useState([]);
+  useEffect(() => {
+    axios({
+      url: "https://api.delta.exchange/v2/products",
+      method: "GET"
+    })
+      .then((res) => {
+        setRowData(res.data.result);
+      })
 
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  useEffect(() => {
+    let symboles = [];
+    rowData.forEach((value) => {
+      symboles.push(value.symbol);
+    });
+    // console.log(symboles)
+    let data = {
+      type: "subscribe",
+      payload: {
+        channels: [
+          {
+            name: "v2/ticker",
+            symbols: symboles
+          }
+        ]
+      }
+    };
 
-        })
-            // Handle the response from backend here
-            .then((res) => { setRowData(res.data.result) })
+    // ws.onopen = () => {
+    //     if (symboles.length>0)  {
+    //     ws.send(JSON.stringify(data))   }
+    // }
 
-            // Catch errors if any
-            .catch((err) => { console.log(err) });
+    // ws.onmessage = evt => {
+    //     const message = JSON.parse(evt.data)
+    //     // console.log(message)
+    // }
+  }, [rowData]);
 
-        ws.onopen = () => {
-            console.log("I m open")
-            ws.send(JSON.stringify({ "name": "v2/ticker", "symbols": ["BTCUSD", "BTCUSDT"] }))
-        }
+  const [columnDefs] = useState([
+    { field: "symbol" },
+    { field: "description" },
+    { field: "underlying_asset.symbol" },
+    { field: "market" }
+  ]);
 
-        ws.onmessage = evt => {
-            console.log("I m")
-            // listen to data sent from the websocket server
-            const message = JSON.parse(evt.data)
-            // this.setState({dataFromServer: message})
-            console.log(message)
-        }
-    }, [])
-    // useEffect(()=>{
-    //     ws.onmessage = evt => {
-    //         // listen to data sent from the websocket server
-    //         const message = JSON.parse(evt.data)
-    //         // this.setState({dataFromServer: message})
-    //         console.log(message)
-    //     }
-    // })
-    const [rowData, setRowData] = useState([
-    ]);
-
-    const [columnDefs] = useState([
-        { field: 'symbol' },
-        { field: 'description' },
-        { field: 'underlying_asset.symbol' }
-    ])
-
-    return (
-        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-            <AgGridReact
-                rowData={rowData}
-                columnDefs={columnDefs}>
-            </AgGridReact>
-        </div>
-    );
+  return (
+    <div
+      className="ag-theme-alpine"
+      style={{ height: 400, width: 800, margin: "auto" }}
+    >
+      {console.log(rowData)}
+      <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+    </div>
+  );
 };
 export default Datatable;
